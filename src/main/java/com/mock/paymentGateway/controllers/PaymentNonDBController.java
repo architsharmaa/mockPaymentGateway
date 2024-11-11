100d35bf93149445cd84d5517aa950262499db7b
Archit Sharma <archit04.sharma@gmail.com>
2024-11-11 12:56:33 +0530

    Add non DB route payment/v2

--- src/main/java/com/mock/paymentGateway/controllers/PaymentNonDBController.java
+++ src/main/java/com/mock/paymentGateway/controllers/PaymentNonDBController.java
@@ -1,8 +1,16 @@
 package com.mock.paymentGateway.controllers;
 
 import com.mock.paymentGateway.models.Payment;
+import com.mock.paymentGateway.repositories.PaymentRepository;
+import com.mock.paymentGateway.services.MockFINetwork;
+import com.mock.paymentGateway.services.PaymentValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 
 /**
@@ -12,6 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/payment/v2")
 public class PaymentNonDBController {
 
-    private Payment payment;
+    @Autowired
+    private PaymentRepository paymentRepository;
+    @Autowired
+    private MockFINetwork mockFINetwork;
+
+    @PostMapping
+    public Payment createDBResponse(@RequestBody Payment payment) {
+
+        payment.setPaymentDate(new Date());
+
+        //validate payments before proceeding for multiple standards
+        if(PaymentValidator.validatePayment(payment)) {
+            payment.setPaymentStatus(mockFINetwork.processPaymentMethod(payment.getPaymentMethod()).toString());
+            return payment;
+        }
+
+        //throw invalidated payment payload
+        payment.setPaymentStatus("Invalid Payment");
+        return payment;
+    }
 
 }
